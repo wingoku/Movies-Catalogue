54311e5ca8e6bbfd0c8f5b11c4c92b5dd96351a8
Umer Farooq <umer.farooq@picavi.com>
2021-02-18 13:40:25 +0100

    Added: application class & added @HiltAndroidApp

---
+++ app/src/main/java/com/wingoku/moviescatalogue/presentation/application/MoviesApplication.java
@@ -0,0 +1,9 @@
+package com.wingoku.moviescatalogue.presentation.application;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MoviesApplication extends Application {
+}

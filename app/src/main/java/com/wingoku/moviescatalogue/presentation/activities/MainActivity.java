9e35400f044f0ef71188a8553277128267c87418
Umer Farooq <umer.farooq@picavi.com>
2021-02-18 17:13:04 +0100

    Added: ui util methods in activity for fragments

--- app/src/main/java/com/wingoku/moviescatalogue/presentation/activities/MainActivity.java
+++ app/src/main/java/com/wingoku/moviescatalogue/presentation/activities/MainActivity.java
@@ -1,8 +1,15 @@
 package com.wingoku.moviescatalogue.presentation.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.wingoku.moviescatalogue.R;
 import com.wingoku.moviescatalogue.data.network.interfaces.MoviesApi;
@@ -12,10 +19,38 @@ import javax.inject.Inject;
 import dagger.hilt.android.AndroidEntryPoint;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ProgressBar progressBar;
+    private Toolbar toolbar;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        toolbar = findViewById(R.id.toolbar);
     }
+
+    public void showToolbar() {
+        toolbar.setVisibility(View.VISIBLE);
+    }
+
+    public void hideToolbar() {
+        toolbar.setVisibility(View.GONE);
+    }
+
+    public void setToolbarTitle(String title) {
+        toolbar.setTitle(title);
+    }
+
+    public void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    public void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
 }

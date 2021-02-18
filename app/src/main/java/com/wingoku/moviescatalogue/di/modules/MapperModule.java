6c595ddd07e4c3d4f01b50700c1ebc7891e0d583
Umer Farooq <umer.farooq@picavi.com>
2021-02-18 13:38:16 +0100

    Added: @installIn(ViewModelComponent) for hilt

--- app/src/main/java/com/wingoku/moviescatalogue/di/modules/MapperModule.java
+++ app/src/main/java/com/wingoku/moviescatalogue/di/modules/MapperModule.java
@@ -7,8 +7,11 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ViewModelComponent;
 
 @Module
+@InstallIn(ViewModelComponent.class)
 public class MapperModule {
     @Provides
     @Singleton

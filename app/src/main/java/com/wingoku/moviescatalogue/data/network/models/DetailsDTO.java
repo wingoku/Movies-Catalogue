ea9be2e37c9604a519124ea84c524dfadef2d475
Umer Farooq <umer.farooq@picavi.com>
2021-02-18 11:34:41 +0100

    Added: DTO models

---
+++ app/src/main/java/com/wingoku/moviescatalogue/data/network/models/DetailsDTO.java
@@ -0,0 +1,75 @@
+package com.wingoku.moviescatalogue.data.network.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class DetailsDTO implements Parcelable {
+
+    @SerializedName("movie_id")
+    @Expose
+    private int movieId;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("sub_title")
+    @Expose
+    private String subTitle;
+    public final static Parcelable.Creator<DetailsDTO> CREATOR = new Creator<DetailsDTO>() {
+        @SuppressWarnings({
+                "unchecked"
+        })
+        public DetailsDTO createFromParcel(Parcel in) {
+            return new DetailsDTO(in);
+        }
+
+        public DetailsDTO[] newArray(int size) {
+            return (new DetailsDTO[size]);
+        }
+    };
+
+    protected DetailsDTO(Parcel in) {
+        this.movieId = ((int) in.readValue((int.class.getClassLoader())));
+        this.title = ((String) in.readValue((String.class.getClassLoader())));
+        this.subTitle = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    public DetailsDTO() {
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(movieId);
+        dest.writeValue(title);
+        dest.writeValue(subTitle);
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+}

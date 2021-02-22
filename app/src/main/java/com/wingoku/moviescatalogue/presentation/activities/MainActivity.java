6c1aa41c2ef9d8e33846f2787d4a2fa43c221a8b
Umer Farooq <umer.farooq@picavi.com>
2021-02-22 11:55:27 +0100

    Added: top level fragments in navigation graph

--- app/src/main/java/com/wingoku/moviescatalogue/presentation/activities/MainActivity.java
+++ app/src/main/java/com/wingoku/moviescatalogue/presentation/activities/MainActivity.java
@@ -18,6 +18,7 @@ import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ProgressBar progressBar;
@@ -31,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
 
         progressBar = findViewById(R.id.progressBar);
         toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initNavController();
+    }
+
+    private void initNavController() {
+        //to make splash screen fragment and categories fragment as TOP LEVEL FRAGMENTS cuz we don't want to show the BACK button in toolbar
+        //https://developer.android.com/guide/navigation/navigation-ui#appbarconfiguration
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.splashScreenFragment, R.id.moviesListFragment).build();
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = NavHostFragment.findNavController(navHostFragment);
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     }
 
     public void showToolbar() {
